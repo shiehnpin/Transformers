2177874b2580ec392d2e8886c4aeb5503feeb583
Hsieh Enping <shieh.npin@gmail.com>
2020-10-19 09:56:35 +0800

    feat(Transformer): add Transformer data class and add test for `create` func.

---
+++ app/src/main/java/com/enping/transformers/data/model/Transformer.kt
@@ -0,0 +1,81 @@
+package com.enping.transformers.data.model
+
+import com.google.gson.annotations.SerializedName
+
+enum class Team(val key: String) {
+    Autobots("A"), Decepticons("D")
+}
+
+data class Transformer(
+    @SerializedName("id")
+    val id: String = "",
+    @SerializedName("name")
+    val name: String = "",
+    @SerializedName("strength")
+    val strength: Int = MIN_RANK,
+    @SerializedName("intelligence")
+    val intelligence: Int = MIN_RANK,
+    @SerializedName("speed")
+    val speed: Int = MIN_RANK,
+    @SerializedName("endurance")
+    val endurance: Int = MIN_RANK,
+    @SerializedName("rank")
+    val rank: Int = MIN_RANK,
+    @SerializedName("courage")
+    val courage: Int = MIN_RANK,
+    @SerializedName("firepower")
+    val firepower: Int = MIN_RANK,
+    @SerializedName("skill")
+    val skill: Int = MIN_RANK,
+    @SerializedName("team")
+    val team: String = "",
+    @SerializedName("team_icon")
+    val teamIcon: String = ""
+) {
+    val rating: Int
+        get() = strength +
+                intelligence +
+                speed +
+                endurance +
+                firepower
+
+    companion object {
+        const val MAX_RANK = 10
+        const val MIN_RANK = 1
+
+        fun create(
+            name: String,
+            strength: Int,
+            intelligence: Int,
+            speed: Int,
+            endurance: Int,
+            rank: Int,
+            courage: Int,
+            firepower: Int,
+            skill: Int,
+            team: Team
+        ): Transformer {
+
+            listOf(
+                strength,
+                intelligence,
+                speed,
+                endurance,
+                rank,
+                courage,
+                firepower,
+                skill
+            ).forEach { rating ->
+                check(rating in MIN_RANK..MAX_RANK) {
+                    "rating is out of range[1..10]"
+                }
+            }
+
+            return Transformer(
+                "", name,
+                strength, intelligence, speed, endurance,
+                rank, courage, firepower, skill, team.key
+            )
+        }
+    }
+}
---
+++ app/src/test/java/com/enping/transformers/data/model/TransformerTest.kt
@@ -0,0 +1,121 @@
+package com.enping.transformers.data.model
+
+import com.enping.transformers.data.model.Transformer.Companion.MAX_RANK
+import com.enping.transformers.data.model.Transformer.Companion.MIN_RANK
+import com.google.common.truth.Truth
+import org.junit.Test
+
+internal class TransformerTest {
+
+    @Test
+    fun `given player when create new Autobots transformer then get new one`() {
+        val expected = Transformer(
+            name = "Optimus Prime",
+            strength = MIN_RANK,
+            intelligence = MIN_RANK,
+            speed = MIN_RANK,
+            endurance = MIN_RANK,
+            rank = MIN_RANK,
+            courage = MIN_RANK,
+            firepower = MIN_RANK,
+            skill = MIN_RANK,
+            team = "A"
+        )
+
+        val actual = Transformer.create(
+            name = "Optimus Prime",
+            strength = MIN_RANK,
+            intelligence = MIN_RANK,
+            speed = MIN_RANK,
+            endurance = MIN_RANK,
+            rank = MIN_RANK,
+            courage = MIN_RANK,
+            firepower = MIN_RANK,
+            skill = MIN_RANK,
+            team = Team.Autobots
+        )
+
+        Truth.assertThat(actual).isEqualTo(expected)
+        Truth.assertThat(actual.rating)
+            .isEqualTo(
+                expected.strength +
+                        expected.intelligence +
+                        expected.speed +
+                        expected.endurance +
+                        expected.firepower
+            )
+    }
+
+
+    @Test
+    fun `given player when create new Decepticons transformer then get new one`() {
+        val expected = Transformer(
+            name = "Predaking",
+            strength = MAX_RANK,
+            intelligence = MAX_RANK,
+            speed = MAX_RANK,
+            endurance = MAX_RANK,
+            rank = MAX_RANK,
+            courage = MAX_RANK,
+            firepower = MAX_RANK,
+            skill = MAX_RANK,
+            team = "D"
+        )
+
+        val actual = Transformer.create(
+            name = "Predaking",
+            strength = MAX_RANK,
+            intelligence = MAX_RANK,
+            speed = MAX_RANK,
+            endurance = MAX_RANK,
+            rank = MAX_RANK,
+            courage = MAX_RANK,
+            firepower = MAX_RANK,
+            skill = MAX_RANK,
+            team = Team.Decepticons
+        )
+
+        Truth.assertThat(actual).isEqualTo(expected)
+        Truth.assertThat(actual.rating)
+            .isEqualTo(
+                expected.strength +
+                        expected.intelligence +
+                        expected.speed +
+                        expected.endurance +
+                        expected.firepower
+            )
+    }
+
+    @Test(expected = IllegalStateException::class)
+    fun `given player when create transformer with over rank then throw exception`() {
+        Transformer.create(
+            name = "Predaking",
+            strength = MAX_RANK + 1,
+            intelligence = MAX_RANK,
+            speed = MAX_RANK,
+            endurance = MAX_RANK,
+            rank = MAX_RANK,
+            courage = MAX_RANK,
+            firepower = MAX_RANK,
+            skill = MAX_RANK,
+            team = Team.Decepticons
+        )
+    }
+
+    @Test(expected = IllegalStateException::class)
+    fun `given player when create transformer with under rank then throw exception`() {
+        Transformer.create(
+            name = "Predaking",
+            strength = MIN_RANK - 1,
+            intelligence = MAX_RANK,
+            speed = MAX_RANK,
+            endurance = MAX_RANK,
+            rank = MAX_RANK,
+            courage = MAX_RANK,
+            firepower = MAX_RANK,
+            skill = MAX_RANK,
+            team = Team.Decepticons
+        )
+    }
+
+}
