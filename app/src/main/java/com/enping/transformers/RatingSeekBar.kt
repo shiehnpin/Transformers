20037fd16ad695ceb0049f63cfab3a1f8d533b48
Hsieh Enping <shieh.npin@gmail.com>
2020-10-20 12:58:25 +0800

    feat(view): add custom seekbar view.

---
+++ app/src/main/java/com/enping/transformers/RatingSeekBar.kt
@@ -0,0 +1,67 @@
+package com.enping.transformers
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.View
+import android.widget.LinearLayout
+import android.widget.SeekBar
+import android.widget.TextView
+import kotlinx.android.synthetic.main.layout_rating_seekbar.view.*
+
+class RatingSeekBar @JvmOverloads constructor(
+    context: Context,
+    attrs: AttributeSet? = null,
+    defStyleAttr: Int = 0
+) : LinearLayout(context, attrs, defStyleAttr) {
+    private val root = View.inflate(context, R.layout.layout_rating_seekbar, this)
+    private val tvValue: TextView
+    private val tvTitle: TextView
+    private val skbRating: SeekBar
+
+    private val offset = 1
+    private var value = 1
+    val ratingName: String
+    var onRatingChangeListener: (Int) -> Unit = {}
+
+    init {
+        val attributes = attrs?.let {
+            context.theme.obtainStyledAttributes(
+                attrs, R.styleable.RatingSeekBar, defStyleAttr, 0
+            )
+        }
+        ratingName = attributes?.getString(R.styleable.RatingSeekBar_ratingName) ?: "unknown"
+        attributes?.recycle()
+
+        tvTitle = root.tv_rating_name_rating_seekbar
+        skbRating = root.skb_rating_value_rating_seekbar
+        tvValue = root.tv_rating_value_rating_seekbar
+
+        tvTitle.text = ratingName.capitalize()
+        skbRating.setOnSeekBarChangeListener(object :
+            SeekBar.OnSeekBarChangeListener {
+            override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
+                value = progress + offset
+                update()
+                if(fromUser) onRatingChangeListener(value)
+            }
+
+            override fun onStartTrackingTouch(seekBar: SeekBar?) {
+            }
+
+            override fun onStopTrackingTouch(seekBar: SeekBar?) {
+            }
+        })
+        update()
+    }
+
+    fun setRating(rating: Int) {
+        value = rating
+        update()
+    }
+
+    private fun update() {
+        skbRating.progress = value - offset
+        tvValue.text = value.toString()
+    }
+
+}
